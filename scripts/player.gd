6f0959e1d1d3316ac5b1d9f949d4d03f46429d7b
Denis <denischebac444@gmail.com>
2025-07-30 04:56:30 +0200

    added bunch of tiles, flame and player

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -700.0
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
@@ -21,5 +22,13 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	# Animations
+	if is_on_floor():
+		if velocity.x == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	animated_sprite.flip_h = velocity.x < 0
 
 	move_and_slide()
