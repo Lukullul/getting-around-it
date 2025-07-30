d8d7fa2449036030edfaf4d87abad2c65873ea42
Lukullul <Lukullul@github.de>
2025-07-30 20:21:58 +0200

    refacture fligth controll

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,25 +12,9 @@ const JUMP_VELOCITY = -700.0
 
 func _physics_process(delta: float) -> void:
 	if (fly):
-		_handle_flight()
+		_handle_flight(delta)
 	else:
-
-		# Add the gravity.
-		if not is_on_floor():
-			velocity += get_gravity() * delta
-
-		# Handle jump.
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
-
-		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
-		var direction := Input.get_axis("ui_left", "ui_right")
-		if direction:
-			velocity.x = direction * SPEED
-			animated_sprite.flip_h = velocity.x < 0
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+		_handle_movement(delta)
 	
 	# Animations
 	if is_on_floor():
@@ -42,17 +26,35 @@ func _physics_process(delta: float) -> void:
 		animated_sprite.play("jump")
 
 	move_and_slide()
+	
+func _handle_movement(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 
-func _handle_flight():
-	velocity = Vector2.ZERO
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+		animated_sprite.flip_h = velocity.x < 0
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+func _handle_flight(delta):
+	velocity = velocity / 1.1
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if (direction):
-		velocity.x = direction * SPEED
+		velocity.x = direction * SPEED * delta * 200
 		animated_sprite.flip_h = velocity.x < 0
 	
 	direction = Input.get_axis("ui_up", "ui_down")
 	if (direction):
-		velocity.y = direction * SPEED
+		velocity.y = direction * SPEED * delta * 200
 		animated_sprite.flip_h = velocity.x < 0
 		
 
