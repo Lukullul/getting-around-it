93aa9f85d2aa97f09cebf220cd1ff2cbc0631184
Denis <denischebac444@gmail.com>
2025-07-31 11:58:46 +0200

    fireball tweak

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,12 +8,12 @@ const JUMP_VELOCITY = -800.0
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 var attacking := false
-
+var facing := 1
 
 func _physics_process(delta: float) -> void:
-	if (fly):
+	if fly:
 		_handle_flight(delta)
-	else:
+	if not fly:
 		_handle_movement(delta)
 		
 	# Attacking
@@ -88,6 +88,8 @@ func _handle_movement(delta):
 		animated_sprite.flip_h = velocity.x < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if direction:
+		facing = direction
 
 func _handle_flight(delta):
 	velocity = velocity / 1.1
@@ -100,7 +102,6 @@ func _handle_flight(delta):
 	if (direction):
 		velocity.y = direction * SPEED * delta * 200
 		animated_sprite.flip_h = velocity.x < 0
-		
 
 
 func _on_flame_hit() -> void:
@@ -109,8 +110,6 @@ func _on_flame_hit() -> void:
 
 func _on_timer_timeout() -> void:
 	attacking = false
-
-
 
 
 func _on_sword_attack_area_area_entered(area: Area2D) -> void:
